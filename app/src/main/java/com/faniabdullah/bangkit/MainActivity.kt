051e1dbc6957003814bd1beabc348400f4ce7287
faniabdullah <faniabdullah27@gmail.com>
2021-03-02 22:09:41 +0700

    add feature menu

--- app/src/main/java/com/faniabdullah/bangkit/MainActivity.kt
+++ app/src/main/java/com/faniabdullah/bangkit/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.faniabdullah.bangkit
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,5 +24,26 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+            }
+            R.id.action_grid -> {
+            }
+            R.id.action_cardview -> {
+            }
+        }
     }
 }
