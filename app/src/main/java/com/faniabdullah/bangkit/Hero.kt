4265a3c4e6687721032ff57161f29c47d4b80810
faniabdullah <faniabdullah27@gmail.com>
2021-03-02 22:03:21 +0700

    practice RecyleViews mode list

---
+++ app/src/main/java/com/faniabdullah/bangkit/Hero.kt
@@ -0,0 +1,7 @@
+package com.faniabdullah.bangkit
+
+data class Hero(
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
--- app/src/main/java/com/faniabdullah/bangkit/MainActivity.kt
+++ app/src/main/java/com/faniabdullah/bangkit/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.faniabdullah.bangkit
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
 
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
